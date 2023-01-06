7b147b6ecbc0e82063e7b81842a6bb6e84e6a7e8
Kunzmann,Kevin (MED BDS) BIP-DE-I <kevin.kunzmann@boehringer-ingelheim.com>
2023-01-06 17:40:08 +0100

    ...

--- install_jags.sh
+++ install_jags.sh
@@ -2,7 +2,7 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-JAGS_HOME=/usr/bin/jags
+JAGS_HOME=/usr/local/jags
 
 mkdir $JAGS_HOME
 wget -O JAGS-4.3.1.tar.gz --no-check-certificate 'https://downloads.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.3.1.tar.gz?ts=gAAAAABiaVHglHhNp4BZaZHGfBZlKKFYGZ3MgKOMq2xGKxPr-dR22veWhMnaWFGHVkzpo-x_-GPgJEwjDSZubU3xt8aBfLddtQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmcmc-jags%2Ffiles%2FJAGS%2F4.x%2FSource%2FJAGS-4.3.1.tar.gz%2Fdownload'
