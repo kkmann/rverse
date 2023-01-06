ac1b24cfcf125908857bceca370565b0a9d9b4b2
Kunzmann,Kevin (MED BDS) BIP-DE-I <kevin.kunzmann@boehringer-ingelheim.com>
2023-01-06 15:57:43 +0100

    ...

--- install_jags.sh
+++ install_jags.sh
@@ -3,16 +3,4 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-# a function to install apt packages only if they are not installed
-# see https://github.com/rocker-org/rocker-versioned2/blob/23b6961dcc187b7290b35ae4434180dfb1fa7f24/scripts/install_verse.sh#L14-L21
-function apt_install() {
-    if ! dpkg -s "$@" >/dev/null 2>&1; then
-        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-            apt-get update
-        fi
-        apt-get install -y --no-install-recommends "$@"
-    fi
-}
-
-apt_install \
-    jags
+apt-get install -y jags
